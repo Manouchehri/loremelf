0a5cd804e5ea895069e51eace27ec89c14f656a7
David Manouchehri <david.manouchehri@riseup.net>
2017-05-02 23:02:18 -0400

    WIP.

--- coder.py
+++ coder.py
@@ -2,25 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import pfp, pfp.fuzz
-import sys
 import IPython
 
-# file = None
-
+# import sys
 # if __name__ == "__main__":
-# 	pass
 # 	#file = sys.argv[1]
 # 	#print file
 
 file = "input/date"
 
 dom = pfp.parse(
-	#keep_successful=True,
-	# predefines=None,
-	#printf=False,
-	# interp=None,
-	#debug=True,
-	# int3=None,
 	data_file = file,
 	template_file = "ELF.bt"
 )
@@ -32,19 +23,21 @@ class IntegersOnly(pfp.fuzz.StratGroup):
 
 	class IntStrat(pfp.fuzz.FieldStrat):
 		klass = pfp.fields.IntBase
-		choices = [0, 1, 2, 3]
+		choices = [xrange(10)]
 
 	def filter_fields(self, fields):
-	 	return filter(lambda x: isinstance(x, pfp.fields.IntBase), fields)
+		return filter(lambda x: isinstance(x, pfp.fields.IntBase), fields)
 
 counter = 0
 
-for mutation in pfp.fuzz.mutate(dom, IntegersOnly, num=10, at_once=10):
+for mutation in pfp.fuzz.mutate(dom, IntegersOnly, num=1, at_once=1):
 	mutated = mutation._pfp__build()
 	filename_out = "output/bin." + str(counter) + ".elf"
-	with open(filename_out, 'w') as file:
-		file.write(mutated)
-	
+	with open(filename_out, 'wb') as file:
+		file.write(bytes(mutated))
+		file.close()
+	m = mutated  # lazy typer.
+	IPython.embed()
 	counter += 1
 #print dom._pfp__error()
 # IPython.embed()
